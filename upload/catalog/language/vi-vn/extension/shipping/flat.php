b97ad83cfb0f597efdf8e266d048c56c52eca198
bambu1310 <cuongxn1310@gmail.com>
2018-10-09 22:10:30 +0700

    init

--- upload/catalog/language/vi-vn/extension/shipping/flat.php
+++ upload/catalog/language/vi-vn/extension/shipping/flat.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_title']       = 'Flat Rate';
-$_['text_description'] = 'Flat Shipping Rate';
+$_['text_title']       = 'Phí vận chuyển';
+$_['text_description'] = 'Phí vận chuyển';
