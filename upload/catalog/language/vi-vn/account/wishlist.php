406ceb9085fdc970f87cfe69a13d73fce81da3d3
huyhuyhuy123 <44017670+huyhuyhuy123@users.noreply.github.com>
2018-10-10 14:21:55 +0700

    Update wishlist.php

--- upload/catalog/language/vi-vn/account/wishlist.php
+++ upload/catalog/language/vi-vn/account/wishlist.php
@@ -5,7 +5,7 @@ $_['heading_title'] = 'Danh sách yêu thích';
 // Text
 $_['text_account']  = 'Tài khoản';
 $_['text_instock']  = 'Còn hàng';
-$_['text_wishlist'] = 'Danh sách yêu thích (%s)';
+$_['text_wishlist'] = 'Danh sách yêu thích';
 $_['text_login']    = 'Bạn phải <a href="%s">đăng nhập</a> hoặc <a href="%s">tạo tài khoản</a> để lưu <a href="%s">%s</a> vào <a href="%s"> danh sách yêu thích</a>!';
 $_['text_success']  = 'Thành công: Bạn vừa thêm <a href="%s">%s</a> vào <a href="%s">danh sách yêu thích</a>!';
 $_['text_remove']   = 'Thành công: Bạn đã chỉnh sửa giỏ hàng!';
